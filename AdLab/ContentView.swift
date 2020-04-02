47e21c91b6c2f8fa80578d390b999294b7675f5e
David Somen <davidsomen@me.com>
2020-04-02 15:22:35 +0900

    Initial Commit

---
+++ AdLab/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  AdLab
+//
+//  Created by David Somen on 02/04/2020.
+//  Copyright © 2020 David Somen. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
